fb7ebf8010964e7b1160caf958f1fcabbfb5ef08
HIRAKU <meishao2002@msn.com>
2017-02-13 19:02:03 +0900

    Update s3.py

--- aws/api/s3.py
+++ aws/api/s3.py
@@ -4,20 +4,20 @@ from flask import Flask, jsonify
 
 class S3Api(object):
     
-    def __init__(self, s3=None, bucket=None):
-        
-        S3_BUCKET = os.environ.get('S3_BUCKET')
-        
-        self.s3 = boto3.resource('s3')
-        self.bucket = s3.Bucket(S3_BUCKET)
+    def __init__(self):
+        pass
     
     def response(self, _filename, _resp):
         
-        PUT_OBJECT_KEY_NAME = _filename
-        obj = self.bucket.Object(PUT_OBJECT_KEY_NAME)
+        S3_BUCKET = os.environ.get('S3_BUCKET')
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(S3_BUCKET)
+        
+        PUT_OBJECT_KEY_NAME = self._filename
+        obj = bucket.Object(PUT_OBJECT_KEY_NAME)
         
         _response = obj.put(
-            Body=_resp,
+            Body=self._resp,
             ContentEncoding='utf-8',
             ContentType='text/csv'
         )
